5eb2b86a7ee751a8fb5e5c20d4ffe9d9c6eaccd3
ecwing <ericcwing@gmail.com>
2019-08-01 15:24:20 -0400

    working axios up and running WIP

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 
 function App() {
+    const [pokedex, setPokedex] = useState([])
+    const [wildPokemon, setWildPokemon] = useState({});
+    
+    useEffect(() => {
+        encounterWildPokemon()
+    }, [])
+
+    const encounterWildPokemon = () => {
+        axios
+        .get('https://pokeapi.co/api/v1/pokemon/151')
+        .then(response => {
+            console.log(response.data);
+            setWildPokemon(response.data);
+        })
+    }
+
     return (
         <div className="app-wrapper">
-        hello world!</div>
+        
+        </div>
     )
 }
 
